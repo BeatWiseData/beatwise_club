17ee5dba82e168efaece073faf83c6e52515a50b
crypto-cooker <mateo.kova0510@gmail.com>
2025-06-30 20:52:49 +0000

    adjust button position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,23 +12,23 @@ export default function Home() {
         <CenteredCard>
           <div className="space-y-6 w-full h-full">
             <div className="flex flex-col h-full w-full">
-              <div className="font-bold text-2xl text-black flex-1 flex flex-row items-center justify-between">
+              <div className="font-bold text-2xl text-black flex-1 flex flex-row items-center justify-between mt-2">
                 <div>
                   <Image src="logo.svg" alt="Logo" width={50} height={50} />
                 </div>
                 <div className="flex flex-row gap-2 sm:gap-3 w-full justify-end">
-                  <button className="w-auto bg-gray-200 text-black py-1 px-2 sm:py-2 sm:px-4 rounded-full sm:ml-3">
+                  <button className="w-auto bg-gray-200 text-black py-1 px-2 sm:py-2 sm:px-4 rounded-full ">
                     <p className="text-[11px] sm:text-sm font-medium">X/Twitter</p>
                   </button>
-                  <button className="w-auto bg-gray-200 text-black py-1 px-2 sm:py-2 sm:px-4 rounded-full sm:ml-3">
+                  <button className="w-auto bg-gray-200 text-black py-1 px-2 sm:py-2 sm:px-4 rounded-full ">
                     <p className="text-[11px] sm:text-sm font-medium">Telegram</p>
                   </button>
-                  <button className="w-auto bg-gray-200 text-black py-1 px-2 sm:py-2 sm:px-4 rounded-full sm:ml-3">
+                  <button className="w-auto bg-gray-200 text-black py-1 px-2 sm:py-2 sm:px-4 rounded-full">
                     <p className="text-[11px] sm:text-sm font-medium">GitHub</p>
                   </button>
                 </div>
               </div>
-              <div className=" flex-3 mt-4">
+              <div className=" flex-2 mt-5">
                 <p className="text-black text-4xl text-5xl-at-500 font-medium text-left">
                   Unlock the Value <br/>
                   of Your Listening
@@ -37,7 +37,7 @@ export default function Home() {
                   Vibin&apos; makes your music data work for you, not for platforms. Private. Secure. Yours.
                 </p>
               </div>
-              <div className="font-bold text-2xl text-black flex-1 flex flex-row gap-2 justify-between mt-6 mb-3">
+              <div className="font-bold text-2xl text-black flex-1 flex flex-row gap-2 justify-between mt-6 mb-1">
                 <button className="flex-1 bg-gray-200 text-black py-2 sm:py-1 px-4 rounded-l-full rounded-r-none text-left">
                   <p className="text-xs sm:text-sm font-medium">Invitation Code</p>
                 </button>
